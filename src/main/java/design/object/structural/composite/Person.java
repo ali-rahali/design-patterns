0058e95b8a2490877db69ea93f4c0e9c752dbf07
Valentine Shemyako <v.shemyako@gmail.com>
2018-06-09 15:02:28 +0300

    Added implementation of composite design pattern

---
+++ src/main/java/design/object/structural/composite/Person.java
@@ -0,0 +1,8 @@
+package design.object.structural.composite;
+
+/**
+ * Defines an interface for client usage
+ */
+public interface Person {
+    void work();
+}
---
+++ src/main/java/design/object/structural/composite/Worker.java
@@ -0,0 +1,12 @@
+package design.object.structural.composite;
+
+/**
+ * It's a leaf in compose design pattern definition
+ */
+public class Worker implements Person {
+
+    @Override
+    public void work() {
+        // some work which is performed by Worker itself
+    }
+}
